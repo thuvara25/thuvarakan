fe1ff793650dc7c41cd9f9bb15bb9f6920dd5e26
thuvara25 <thuvarakant2@gmail.com>
2019-03-06 15:21:49 +0530

    add code

--- app/src/main/java/lk/ac/kln/countdowntimer/MainActivity.java
+++ app/src/main/java/lk/ac/kln/countdowntimer/MainActivity.java
@@ -1,13 +1,25 @@
 package lk.ac.kln.countdowntimer;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+private int counter =99;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        count_down();
     }
-}
+private void count_down(){
+    final TextView textview= findViewById(R.id."+id/textView" )_;
+    final Handler handler = new handler();
+    handler.post(new Runnable(){
+        @Override
+        public void run (){
+                textview.setText(Integer.toString(counter));
+                counter--;
+                handler.postDelayed(r.this_delayMillis:1000_);
